4f25da1066c1eac418109e1ee41bbfdbb6b16b8b
jonathanlbr <jonathanllima016@gmail.com>
2024-08-21 14:01:55 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,12 @@
+a = int(input('digite um numero'))
+s = str(input('digite o sinal'))
+b = int(input('digite um numero'))
+
+if(s =='+'):
+    print(a+b)
+elif (s == '-'):
+    print(a-b)
+elif (s == '*'):
+    print(a*B)
+else:
+    print(a/b)
